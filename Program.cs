a672436c630d8bbbbfc1c05f83b108a7d5283f4b
DaySet909 <78254936+DaySet909@users.noreply.github.com>
2022-05-31 00:49:54 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //как тебя зовут, сколько тебе лет, какого цвета у тебя волосы, какого цвета глаза 
+            string name;
+            string age;
+            string colorHair;
+            string colorEyes;
+
+            Console.WriteLine("Как тебя зовут?");
+            name = Console.ReadLine();
+            Console.WriteLine("Сколько тебе лет?");
+            age = Console.ReadLine();
+            Console.WriteLine("Какого цвета твои волосы?");
+            colorHair = Console.ReadLine();
+            Console.WriteLine("Какого цвета твои глаза?");
+            colorEyes = Console.ReadLine();
+
+            Console.WriteLine("Здравствуй, {0}, тебе {1}, у тебя {2} волосы и {3} глаза", name, age, colorHair, colorEyes);
+        }
+    }
+}
